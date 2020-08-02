012adf0ac2ed39f3be07d0eb583d943027ca8a91
Phil King <phil@beardedphil.com>
2020-08-02 14:43:50 -0500

    Preparing to deploy to heroku.

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -3,6 +3,8 @@ import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 import ExpensesSummary from './ExpensesSummary';
 
+console.log('aldkf');
+
 const ExpenseDashboardPage = () => (
   <div>
     <ExpensesSummary />
